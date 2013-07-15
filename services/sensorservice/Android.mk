61796f44720673d86b4fa4b77ee6896e63278e46
6b72 <kastur@gmail.com>
2013-07-14 23:17:02 -0400

    Added an executable that will create the firewall config file.
    Change-Id: Ia486a27451805698426cfae410bc935fb738e00a

--- services/sensorservice/Android.mk
+++ services/sensorservice/Android.mk
@@ -39,3 +39,28 @@ LOCAL_C_INCLUDES := \
 	external/stlport/stlport \
 
 include $(BUILD_SHARED_LIBRARY)
+
+# Build a host executable for creating firewall configs.
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	FirewallConfig.proto \
+	FirewallConfig.cpp \
+
+LOCAL_CFLAGS:= -DLOG_TAG=\"SensorFirewall-Config\"
+
+LOCAL_MODULE:= sensorfirewall-cofig
+
+LOCAL_SHARED_LIBRARIES := \
+	libcutils \
+	libstlport \
+
+LOCAL_STATIC_LIBRARIES := \
+	libprotobuf-cpp-2.3.0-full
+
+LOCAL_C_INCLUDES := \
+	bionic \
+	bionic/libstdc++/include \
+	external/stlport/stlport \
+
+include $(BUILD_EXECUTABLE)
