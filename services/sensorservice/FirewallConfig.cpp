bca5921e9d9941e37dd6b6b9abc9fb4cd19bc10c
6b72 <kastur@gmail.com>
2013-07-19 23:23:07 -0400

    BUGFIX: zero_copy_stream fof {i,o}fstram is for 'full' protobuf only.
    Also changed typo name from 'sensor-cofig' to 'sensor-config'
    
    Test with this, and monitor adb logcat
    ```
    make -j8 sensorfirewall-cofing  && adb push
    out/target/product/mako/system/bin/sensorfirewall-cofig
    /data/bin/sf-config && adb root && adb remount &&  adb shell
    '/data/bin/sf-config' && adb shell 'ls -l /etc/firewall-config'
    ```
    
    SensorService.cpp is still broken, have to fix. Probably won't compile
    
    Change-Id: I47d1473393ae0a8af3041222c89fd0898c367081

--- services/sensorservice/FirewallConfig.cpp
+++ services/sensorservice/FirewallConfig.cpp
@@ -3,8 +3,6 @@
 
 #include <cutils/log.h>
 
-#include <google/protobuf/io/zero_copy_stream_impl.h>
-
 #include "frameworks/native/services/sensorservice/FirewallConfigMessages.pb.h"
 
 using android_sensorfirewall::FirewallConfig;
@@ -13,14 +11,38 @@ using android_sensorfirewall::Action;
 
 const char* kFirewallConfigFileName = "/etc/firewall-config";
 
+bool get_file_contents(const char *filename, std::string* contents) {
+  std::string& local = *contents;
+  std::ifstream ifs(filename, std::ios::in | std::ios::binary);
+  if (!ifs)
+    return false;
+  ifs.seekg(0, std::ios::end);
+  local.resize(ifs.tellg());
+  ifs.seekg(0, std::ios::beg);
+  ifs.read(&local[0], local.size());
+  ifs.close();
+  return true;
+}
+
+void PrintFirewallConfig(const FirewallConfig& firewallConfig) {
+    int i;
+    for (int ii = 0; ii < firewallConfig.rule_size(); ++ii) {
+        const Rule& rule = firewallConfig.rule(ii);
+        ALOGD("rule_name = %s: sensorType = %d: pkgName = %s: pkgUid = %d:",
+            rule.rulename().c_str(), rule.sensortype(), rule.pkgname().c_str(),
+            rule.pkguid());
+        ALOGD("actionType = %d", rule.action().actiontype());
+    }
+}
+
 int main(int argc, char** argv) {
     ALOGD("=-=-=-=-= FIREWALL CONFIG =-=-=-=-=");
 
-    FirewallConfig firewallConfig;
+    FirewallConfig firewallConfig_out;
     Rule* rule = NULL;
     Action* action = NULL;
 
-    rule = firewallConfig.add_rule();
+    rule = firewallConfig_out.add_rule();
     rule->set_rulename("Rule1");
     rule->set_sensortype(1);
     rule->set_pkgname("Facebook.com");
@@ -28,7 +50,7 @@ int main(int argc, char** argv) {
     action = rule->mutable_action();
     action->set_actiontype(Action::ACTION_SUPPRESS);
 
-    rule = firewallConfig.add_rule();
+    rule = firewallConfig_out.add_rule();
     rule->set_rulename("Rule2");
     rule->set_sensortype(2);
     rule->set_pkgname("Twitter.com");
@@ -37,13 +59,7 @@ int main(int argc, char** argv) {
     action->set_actiontype(Action::ACTION_CONSTANT);
 
     ALOGD("====== Writing these rules to /etc/firewall-config====");
-    int i;
-    for( i = 0; i < firewallConfig.rule_size(); i++) {
-        const Rule& pRule = firewallConfig.rule(i);
-        ALOGD("RuleName = %s, sensorType = %d, pkgName = %s, pkguid = %d\n", pRule.rulename().c_str(), pRule.sensortype(), pRule.pkgname().c_str(), pRule.pkguid());
-    }
-
-    //firewallConfig.set_debug_info("EUREKA! AND FOO BAR TO YOU.");
+    PrintFirewallConfig(firewallConfig_out);
 
     std::fstream outputStream(kFirewallConfigFileName,
             std::ios::out | std::ios::binary);
@@ -53,18 +69,46 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    // DebugString() is only supported with 'Message' not 'MessageLite'
-    // ALOGD("Firewall config: %s", firewallConfig.DebugString().c_str());
-
-    google::protobuf::io::OstreamOutputStream zerocopyOutputStream(&outputStream);
-    if (!firewallConfig.SerializeToZeroCopyStream(&zerocopyOutputStream)) {
-        ALOGE("Failed to serialize to file.");
+    std::string serialized_proto_out;
+    if (!firewallConfig_out.SerializeToString(&serialized_proto_out)) {
+        ALOGE("Failed to serialize to string.");
         return -1;
     }
 
+    outputStream << serialized_proto_out;
+
     ALOGD("Flush and close %s", kFirewallConfigFileName);
-    outputStream << std::flush;
+    //outputStream << std::flush;
     outputStream.close();
+
+
+    ALOGD("====== Reading rules from /etc/firewall-config====");
+    std::fstream inputStream(kFirewallConfigFileName,
+            std::ios::in | std::ios::binary);
+
+    if (!inputStream.is_open()) {
+        ALOGE("Failed to open file.");
+        return -1;
+    }
+
+    //TODO(krr): lock the config file while reading via fnctl.
+
+    std::string serialized_proto_in;
+    if (!get_file_contents(kFirewallConfigFileName, &serialized_proto_in)) {
+        ALOGE("Failed to read from file.");
+        return -1;
+    }
+
+    FirewallConfig firewallConfig_in;
+    if (!firewallConfig_in.ParseFromString(serialized_proto_in)) {
+        ALOGE("Failed to parse string.");
+        return -1;
+    }
+
+    inputStream.close();
+
+    PrintFirewallConfig(firewallConfig_in);
+    
 
     return 0;
 }
